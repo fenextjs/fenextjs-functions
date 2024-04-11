c647a41141dbae1d5f5d7b9d079d18b23439750d
Francisco Blanco <blancofrancisco34@gmail.com>
2024-04-11 10:59:27 -0500

    add: parseCSC

---
+++ src/parse/CSC/index.tsx
@@ -0,0 +1,32 @@
+import { CSCProps, CSCStringProps } from "fenextjs-interface";
+import { parseString_to_Country, parseCountry_to_String } from "../Country";
+import { parseString_to_State, parseState_to_String } from "../State";
+import { parseString_to_City, parseCity_to_String } from "../City";
+
+export const parseCSC_to_String = (
+    data: CSCProps | undefined | null,
+): CSCStringProps => {
+    try {
+        return {
+            country: parseCountry_to_String(data?.country),
+            state: parseState_to_String(data?.state),
+            city: parseCity_to_String(data?.city),
+        };
+    } catch {
+        return {};
+    }
+};
+
+export const parseCSCString_to_CSC = (
+    data: CSCStringProps | undefined | null,
+): CSCProps => {
+    try {
+        return {
+            country: parseString_to_Country(data?.country),
+            state: parseString_to_State(data?.state),
+            city: parseString_to_City(data?.city),
+        };
+    } catch {
+        return {};
+    }
+};
