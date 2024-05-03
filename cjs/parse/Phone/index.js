80bd9f70935bf5116448a93ab05a2f100726c717
Francisco Blanco <blancofrancisco34@gmail.com>
2024-05-03 14:36:43 -0500

    flb

--- cjs/parse/Phone/index.js
+++ cjs/parse/Phone/index.js
@@ -15,8 +15,12 @@ const parseString_to_Phone = (data) => {
         return JSON.parse(`${data ?? ""}`);
     }
     catch {
+        const num = `${data}`.replace(/[^1-9-+ ]/g, "");
+        const n = num.split(/[ -]/g).filter((e) => e != "");
         return {
-            number: `${data ?? ""}`,
+            number: n?.pop() ?? "",
+            code: n?.join("-"),
+            tel: num,
         };
     }
 };
--- src/parse/Phone/index.tsx
+++ src/parse/Phone/index.tsx
@@ -16,11 +16,11 @@ export const parseString_to_Phone = (
     try {
         return JSON.parse(`${data ?? ""}`);
     } catch {
-        const num = `${data}`.replace(/[^1-9-+ ]/g, '');
-        const n = num.split(/[ -]/g).filter((e) => e != '');
+        const num = `${data}`.replace(/[^1-9-+ ]/g, "");
+        const n = num.split(/[ -]/g).filter((e) => e != "");
         return {
-            number: n?.pop() ?? '',
-            code: n?.join('-'),
+            number: n?.pop() ?? "",
+            code: n?.join("-"),
             tel: num,
         };
     }
