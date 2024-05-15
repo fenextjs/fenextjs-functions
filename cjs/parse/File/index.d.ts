5c56833675205987e9c7d839f09ceed75efce34f
Francisco Blanco <blancofrancisco34@gmail.com>
2024-05-15 16:07:24 -0500

    5.0.0

--- cjs/parse/File/index.d.ts
+++ cjs/parse/File/index.d.ts
@@ -7,6 +7,6 @@
  * @returns {Promise} - A promise that resolves with the file contents
  */
 export declare const parseFile: (file: any, { updateProgress, fileText, }: {
-    fileText?: boolean;
+    fileText?: boolean | undefined;
     updateProgress: (progress: number) => void;
 }) => Promise<unknown>;
