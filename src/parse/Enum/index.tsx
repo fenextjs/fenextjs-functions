944a9e24b4e38e497f76ac7e761202b98c07d6e7
franciscoblancojn <blancofrancisco34@gmail.com>
2024-08-27 12:31:45 -0500

    parseEnum_to_String_Options

--- src/parse/Enum/index.tsx
+++ src/parse/Enum/index.tsx
@@ -2,8 +2,12 @@ export type parseEnum_to_String_SW<T extends string> = {
     [id in T]: string;
 };
 
+export interface parseEnum_to_String_Options {
+    valueNull?:string
+}
+
 export const parseEnum_to_String =
-    <T extends string>(sw: parseEnum_to_String_SW<T>) =>
+    <T extends string>(sw: parseEnum_to_String_SW<T>,options?:parseEnum_to_String_Options) =>
     (type?: T | null | undefined): string => {
-        return type ? sw[type] : "";
+        return type ? sw[type] : (options?.valueNull ??  "");
     };
