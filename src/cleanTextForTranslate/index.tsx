a37c7e36ad3b52a9031ba32761107a20472cf4fc
franciscoblancojn <blancofrancisco34@gmail.com>
2024-12-03 16:23:12 -0500

    add cleanTextForTranslate

---
+++ src/cleanTextForTranslate/index.tsx
@@ -0,0 +1,8 @@
+
+export const cleanTextForTranslate = (text: string) => {
+    return text
+        .replaceAll("\\n", "")
+        .replaceAll("\n", "")
+        .replace(/ {2,}/g, " ")
+        .trim();
+}
