68eadc6d0f2997e91a2f4d2dd59d158746b86dce
franciscoblancojn <blancofrancisco34@gmail.com>
2024-12-03 16:28:12 -0500

    add doc

---
+++ src/cleanTextForTranslate/_.doc.tsx
@@ -0,0 +1,37 @@
+export default {
+    id: "cleanTextForTranslate",
+    name: "cleanTextForTranslate",
+    description:
+        "La función cleanTextForTranslate permite limpiar un string para poder traducirlo.",
+    props: [
+        {
+            id: "text",
+            type: "string",
+            require: true,
+            description:
+                "Texto a tranducir.",
+        },
+    ],
+    returns: [
+        {
+            id: "text",
+            type: "string",
+            description:
+                "Texto Limpio para traducir.",
+        },
+    ],
+    useExample: [
+        {
+            text: "Limpiar espacios",
+            content: `cleanTextForTranslate("Text          Text2") // Retorna "Text Text2"`,
+        },
+        {
+            text: "Limpiar espacios iniciales o finales",
+            content: `GetCardType(" Text ") // Retorna "Text"`,
+        },
+        {
+            text: "Limpiar saltos de linea",
+            content: `cleanTextForTranslate("Text \\n Text2") // Retorna "Text Text2"`,
+        },
+    ],
+};
