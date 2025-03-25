05ea66912f365d03fe110583721d2583b5f9d467
franciscoblancojn <blancofrancisco34@gmail.com>
2025-03-25 11:08:13 -0500

    flb

--- cjs/config/index.js
+++ cjs/config/index.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CONFIG = void 0;
 exports.CONFIG = {
-    EMPY: process.env["NEXT_PUBLIC_EMPY"] == "TRUE",
-    MODATA: process.env["NEXT_PUBLIC_MODATA"] == "TRUE",
-    LOG: process.env["NEXT_PUBLIC_LOG"] == "TRUE",
+    EMPY: process?.env?.["NEXT_PUBLIC_EMPY"] == "TRUE",
+    MODATA: process?.env?.["NEXT_PUBLIC_MODATA"] == "TRUE",
+    LOG: process?.env?.["NEXT_PUBLIC_LOG"] == "TRUE",
 };
 //# sourceMappingURL=index.js.map
