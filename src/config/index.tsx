3690ea1618fb1a5b07ca487d96d11305f6e6ae85
franciscoblancojn <blancofrancisco34@gmail.com>
2025-03-25 11:07:56 -0500

    change dependencies

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    EMPY: process.env["NEXT_PUBLIC_EMPY"] == "TRUE",
-    MODATA: process.env["NEXT_PUBLIC_MODATA"] == "TRUE",
-    LOG: process.env["NEXT_PUBLIC_LOG"] == "TRUE",
+    EMPY: process?.env?.["NEXT_PUBLIC_EMPY"] == "TRUE",
+    MODATA: process?.env?.["NEXT_PUBLIC_MODATA"] == "TRUE",
+    LOG: process?.env?.["NEXT_PUBLIC_LOG"] == "TRUE",
 };
