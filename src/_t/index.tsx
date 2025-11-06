64443a47f44a112252afcee68c23dbed5f9e6dca
franciscoblancojn <blancofrancisco34@gmail.com>
2025-11-06 17:59:04 -0500

    fix  _tValidate

--- src/_t/index.tsx
+++ src/_t/index.tsx
@@ -1,8 +1,12 @@
 import { _TFunciton } from "fenextjs-interface";
 
 export const _tValidate = (d: any, _t?: _TFunciton) => {
-    if (typeof d == "string" && _t) {
-        return _t?.(d);
+    try {
+        if (typeof d == "string" && _t) {
+            return _t?.(d);
+        }
+        return d;
+    } catch {
+        return d;
     }
-    return d;
 };
