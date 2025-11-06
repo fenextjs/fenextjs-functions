621dbd1ae436b5e357fcd6892b29602e320f9095
franciscoblancojn <blancofrancisco34@gmail.com>
2025-11-06 17:59:20 -0500

    flb

--- cjs/_t/index.js
+++ cjs/_t/index.js
@@ -2,10 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports._tValidate = void 0;
 const _tValidate = (d, _t) => {
-    if (typeof d == "string" && _t) {
-        return _t?.(d);
+    try {
+        if (typeof d == "string" && _t) {
+            return _t?.(d);
+        }
+        return d;
     }
-    return d;
+    catch {
+        return d;
+    }
 };
 exports._tValidate = _tValidate;
 //# sourceMappingURL=index.js.map
